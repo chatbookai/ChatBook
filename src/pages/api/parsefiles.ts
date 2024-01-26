03e5e822cf0df2583afb3fa2ed01ab21904e402b
Chives Network <chivesreact@gmail.com>
2024-01-26 12:03:32 -0700

    ParseFiles

--- src/pages/api/parsefiles.ts
+++ src/pages/api/parsefiles.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { parseFiles } from '../../utils/llms';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const parseFilesData: any = await parseFiles();
+    await parseFiles();
     
     res.status(200).send("Execute finished, logs in the console or the log page");
 }

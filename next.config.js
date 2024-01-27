44fcabc70e642c8fd380191c08b94b31add1a781
Chives Network <chivesreact@gmail.com>
2024-01-26 20:33:50 -0700

    webpack

--- next.config.js
+++ next.config.js
@@ -6,12 +6,26 @@ const path = require('path')
 // Remove this if you're not using Fullcalendar features
 
 module.exports = {
-  trailingSlash: true,
-  reactStrictMode: false,
-  webpack: config => {
+  trailingSlash: false,
+  reactStrictMode: true,
+  webpack(config, { isServer, dev }) {
     config.resolve.alias = {
       ...config.resolve.alias,
       apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
+    }
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true,
+    }
+
+    // 在开发环境中输出更详细的错误信息
+    if (dev) {
+      console.log('Running in development mode');
+    }
+
+    // 在服务器端构建时的特定配置
+    if (isServer) {
+      console.log('Building for server-side');
     }
 
     return config

69dc2580f77ce10a6f0e289066c9cfab4d997eca
Chives Network <chivesreact@gmail.com>
2024-01-26 17:36:25 -0700

    pages/api/test

---
+++ src/pages/api/test.ts
@@ -0,0 +1,6 @@
+// @ts-ignore
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    res.status(200).send('Hello Test!');
+}

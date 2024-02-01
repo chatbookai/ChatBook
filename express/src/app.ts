efbbae85402c4336be38d67f54e1b467ad619b88
Chives Network <chivesreact@gmail.com>
2024-02-01 11:13:04 -0700

    Fix ISSUES

--- express/src/app.ts
+++ express/src/app.ts
@@ -36,8 +36,14 @@ app.post('/api/TTS-1', async (req: Request, res: Response) => {
   const { authorization } = req.headers;
   const checkUserTokenData: any = await checkUserToken(authorization as string);
   if(checkUserTokenData && checkUserTokenData.data && checkUserTokenData.data.email && checkUserTokenData.data.role == 'admin') {
-      const GenereateAudioUsingTTSData = await GenereateAudioUsingTTS(res, "TTS-1", checkUserTokenData.data.id, question, 'alloy');
-      res.status(200).json(GenereateAudioUsingTTSData);
+      const getLLMSSettingData = await getLLMSSetting("TTS-1");   
+      if(getLLMSSettingData && getLLMSSettingData.OPENAI_API_KEY && getLLMSSettingData.OPENAI_API_KEY != "") {
+        const GenereateAudioUsingTTSData = await GenereateAudioUsingTTS(res, "TTS-1", checkUserTokenData.data.id, question, 'alloy');
+        res.status(200).json(GenereateAudioUsingTTSData);
+      }
+      else {        
+        res.status(200).json({"status":"error", "msg":"Not set API_KEY", "data": null});
+      }
   }
   else {
       res.status(200).json({"status":"error", "msg":"Token is invalid", "data": null});
@@ -87,8 +93,14 @@ app.post('/api/DallE2Openai', async (req: Request, res: Response) => {
   const { authorization } = req.headers;
   const checkUserTokenData: any = await checkUserToken(authorization as string);
   if(checkUserTokenData && checkUserTokenData.data && checkUserTokenData.data.email && checkUserTokenData.data.role == 'admin') {
-      const GenereateImageUsingDallE2Data = await GenereateImageUsingDallE2(res, "Dall-E-2", checkUserTokenData.data.id, question, '1024x1024');
-      res.status(200).json(GenereateImageUsingDallE2Data);
+      const getLLMSSettingData = await getLLMSSetting("Dall-E-2");   
+      if(getLLMSSettingData && getLLMSSettingData.OPENAI_API_KEY && getLLMSSettingData.OPENAI_API_KEY != "") {
+        const GenereateImageUsingDallE2Data = await GenereateImageUsingDallE2(res, "Dall-E-2", checkUserTokenData.data.id, question, '1024x1024');
+        res.status(200).json(GenereateImageUsingDallE2Data);
+      }
+      else {        
+        res.status(200).json({"status":"error", "msg":"Not set API_KEY", "data": null});
+      }
   }
   else {
       res.status(200).json({"status":"error", "msg":"Token is invalid", "data": null});
@@ -101,8 +113,14 @@ app.post('/api/ChatOpenaiKnowledge', async (req: Request, res: Response) => {
   const { authorization } = req.headers;
   const checkUserTokenData: any = await checkUserToken(authorization as string);
   if(checkUserTokenData && checkUserTokenData.data && checkUserTokenData.data.email && (checkUserTokenData.data.role == 'admin' || checkUserTokenData.data.role == 'user')) {
-      await chatKnowledgeOpenAI(res, knowledgeId, checkUserTokenData.data.id, question, history);
-      res.end();
+      const getLLMSSettingData = await getLLMSSetting(knowledgeId);   
+      if(getLLMSSettingData && getLLMSSettingData.OPENAI_API_KEY && getLLMSSettingData.OPENAI_API_KEY != "") {
+        await chatKnowledgeOpenAI(res, knowledgeId, checkUserTokenData.data.id, question, history);
+        res.end();
+      }
+      else {        
+        res.status(200).json({"status":"error", "msg":"Not set API_KEY", "data": null});
+      }
   }
   else {
       res.status(200).json({"status":"error", "msg":"Token is invalid", "data": null});
@@ -115,8 +133,14 @@ app.post('/api/ChatOpenai', async (req: Request, res: Response) => {
   const { authorization } = req.headers;
   const checkUserTokenData: any = await checkUserToken(authorization as string);
   if(checkUserTokenData && checkUserTokenData.data && checkUserTokenData.data.email && (checkUserTokenData.data.role == 'admin' || checkUserTokenData.data.role == 'user')) {
-      await chatChatOpenAI(res, knowledgeId, checkUserTokenData.data.id, question, history);
-      res.end();
+      const getLLMSSettingData = await getLLMSSetting(knowledgeId);   
+      if(getLLMSSettingData && getLLMSSettingData.OPENAI_API_KEY && getLLMSSettingData.OPENAI_API_KEY != "") {
+        await chatChatOpenAI(res, knowledgeId, checkUserTokenData.data.id, question, history);
+        res.end();
+      }
+      else {        
+        res.status(200).json({"status":"error", "msg":"Not set API_KEY", "data": null});
+      }
   }
   else {
       res.status(200).json({"status":"error", "msg":"Token is invalid", "data": null});
@@ -129,8 +153,14 @@ app.post('/api/ChatGemini', async (req: Request, res: Response) => {
   const { authorization } = req.headers;
   const checkUserTokenData: any = await checkUserToken(authorization as string);
   if(checkUserTokenData && checkUserTokenData.data && checkUserTokenData.data.email && (checkUserTokenData.data.role == 'admin' || checkUserTokenData.data.role == 'user')) {
-      await chatChatGemini(res, knowledgeId, checkUserTokenData.data.id, question, history);
-      res.end();
+      const getLLMSSettingData = await getLLMSSetting(knowledgeId);   
+      if(getLLMSSettingData && getLLMSSettingData.OPENAI_API_KEY && getLLMSSettingData.OPENAI_API_KEY != "") {
+        await chatChatGemini(res, knowledgeId, checkUserTokenData.data.id, question, history);
+        res.end();
+      }
+      else {        
+        res.status(200).json({"status":"error", "msg":"Not set API_KEY", "data": null});
+      }
   }
   else {
       res.status(200).json({"status":"error", "msg":"Token is invalid", "data": null});
@@ -143,8 +173,14 @@ app.post('/api/ChatBaiduwenxin', async (req: Request, res: Response) => {
   const { authorization } = req.headers;
   const checkUserTokenData: any = await checkUserToken(authorization as string);
   if(checkUserTokenData && checkUserTokenData.data && checkUserTokenData.data.email && (checkUserTokenData.data.role == 'admin' || checkUserTokenData.data.role == 'user')) {
-      const chatChatBaiduWenxinData: any = await chatChatBaiduWenxin(res, knowledgeId, checkUserTokenData.data.id, question, history);    
-      res.status(200).json(chatChatBaiduWenxinData);
+      const getLLMSSettingData = await getLLMSSetting(knowledgeId);   
+      if(getLLMSSettingData && getLLMSSettingData.OPENAI_API_KEY && getLLMSSettingData.OPENAI_API_KEY != "") {
+        const chatChatBaiduWenxinData: any = await chatChatBaiduWenxin(res, knowledgeId, checkUserTokenData.data.id, question, history);    
+        res.status(200).json(chatChatBaiduWenxinData);
+      }
+      else {        
+        res.status(200).json({"status":"error", "msg":"Not set API_KEY", "data": null});
+      }
   }
   else {
       res.status(200).json({"status":"error", "msg":"Token is invalid", "data": null});

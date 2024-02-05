c6fec8ecd667cbcafdc7b6c23a13db403f2e3caf
Chives Network <chivesreact@gmail.com>
2024-02-04 19:52:06 -0700

    Fix Issues

--- express/src/router/getimg.ts
+++ express/src/router/getimg.ts
@@ -3,48 +3,9 @@
 
   import { checkUserToken } from '../utils/user';
 
-  import { getModels, getModelDetail, TextToImageALL, getUserImages, getUserImagesAll, generateImageStabilityAi } from '../utils/getimg';
+  import { getUserImages, getUserImagesAll, generateImageStabilityAi } from '../utils/getimg';
 
   const app = express();
-
-  app.get('/api/getModels', async (req: Request, res: Response) => {
-    const getModelsData = await getModels();
-    console.log("getModels", getModelsData)
-    res.status(200).json(getModelsData).end();
-    /*
-    const { authorization } = req.headers;
-    const checkUserTokenData: any = await checkUserToken(authorization as string);
-    if(checkUserTokenData && checkUserTokenData.data && checkUserTokenData.data.email && checkUserTokenData.data.role == 'admin') {
-        res.status(200).json({});
-    }
-    else if(checkUserTokenData && checkUserTokenData.data && checkUserTokenData.data.email && checkUserTokenData.data.role == 'user') {
-        res.status(200).json({});
-    }
-    else {
-        res.status(200).json({"status":"error", "msg":"Token is invalid", "data": null});
-    }
-    res.end();
-    */
-  });
-
-  app.get('/api/getModelDetail/:id', async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const getModelDetailData = await getModelDetail(id);
-    console.log("getModelDetailData", getModelDetailData)
-    res.status(200).json(getModelDetailData).end();
-  });
-
-  app.get('/api/TextToImageALL', async (req: Request, res: Response) => {
-    const TextToImageALLData = await TextToImageALL();
-    console.log("TextToImageALLData", TextToImageALLData);
-    let TextToImageALLDataHtml = '';
-    TextToImageALLData.map((FileName: string)=>{
-        if(FileName) {
-            TextToImageALLDataHtml += "<img src='/api/image/"+FileName+"' border=0 width=600>";
-        }
-    })
-    res.status(200).send(TextToImageALLDataHtml).end();
-  });
 
   app.post('/api/getUserImages', async (req: Request, res: Response) => {
     const { authorization } = req.headers;

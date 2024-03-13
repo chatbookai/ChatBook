10a105caf8000252b01b9ba1510259334a5997ae
Chives Network <chivesreact@gmail.com>
2024-03-12 19:11:33 -0600

    ChatBook

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -6,7 +6,7 @@ console.log('Hostname:', hostname);
 
 export default {
   productName: 'ChatBook',
-  backEndApiChatBook: (hostname == 'chatbookai.net' || hostname == 'www.chatbookai.net') ? 'https://chatbookai.net' : 'http://localhost:1988',
+  backEndApiChatBook: (hostname == 'localhost') ? 'http://localhost:1988' : 'https://chatbookai.net',
   meEndpoint: '/auth/me',
   storageTokenKeyName: 'ChatBookAccessToken',
   userInfoTokenKeyName: 'ChatBookUserToken',

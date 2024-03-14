f052244bef7136e6532fcb686c2dcee13f3d99d7
Chives Network <chivesreact@gmail.com>
2024-03-13 21:24:05 -0600

    MindElixirReact

--- src/views/chat/MindMap/MindElixirReact.jsx
+++ src/views/chat/MindMap/MindElixirReact.jsx
@@ -1,54 +1,50 @@
-import React, { useEffect, useRef, forwardRef, useState } from 'react'
+import React, { useEffect, useRef, forwardRef } from 'react'
+
+let MindElixir;
+import('mind-elixir').then((module) => { MindElixir = module.default; }).catch((error) => { console.error('Failed to import MindElixir:', error.message); MindElixir = null; });
 
 function MindElixirReact(
   { style, data, options, plugins, onOperate, onSelectNode, onExpandNode },
   ref
 ) {
   const isFirstRun = useRef(true)
-  const isLoaded = useRef(false)
+  
+  useEffect(() => {
+    if(MindElixir != null)     {
+      isFirstRun.current = true
+      const me = new MindElixir({
+        el: ref.current,
+        ...options,
+      })
+      for (let i = 0; i < plugins.length; i++) {
+        const plugin = plugins[i]
+        me.install(plugin)
+      }
+      me.bus.addListener('operation', (operation) => {
+        onOperate(operation)
+      })
+      me.bus.addListener('selectNode', (operation) => {
+        onSelectNode(operation)
+      })
+      me.bus.addListener('expandNode', (operation) => {
+        onExpandNode(operation)
+      })
+      ref.current.instance = me
+      console.log('created', ref.current.instance)
+    }
+  }, [ref, options, plugins, onOperate, onSelectNode, onExpandNode, MindElixir])
 
   useEffect(() => {
     if (isFirstRun.current) {
-      isFirstRun.current = false;
-      import("mind-elixir").then((MindElixirModule) => {
-        const me = new MindElixirModule.default({
-          el: ref.current,
-          ...options,
-        });
-        for (let i = 0; i < plugins.length; i++) {
-          const plugin = plugins[i];
-          me.install(plugin);
-        }
-        me.bus.addListener("operation", (operation) => {
-          onOperate(operation);
-        });
-        me.bus.addListener("selectNode", (operation) => {
-          onSelectNode(operation);
-        });
-        me.bus.addListener("expandNode", (operation) => {
-          onExpandNode(operation);
-        });
-        ref.current.instance = me;
-        console.log("created", ref.current.instance);
-        isLoaded.current = true;
-      });
+      if (!ref.current.instance) return
+      ref.current.instance.init(data)
+      isFirstRun.current = false
+      console.log('init', ref.current.instance)
+    } else {
+      ref.current.instance.refresh(data)
+      console.log('refresh', ref.current.instance)
     }
-  }, [ref, options, plugins, onOperate, onSelectNode, onExpandNode]);
-
-  useEffect(() => {
-    if(isLoaded.current) {
-      if (isFirstRun.current) {
-        if (!ref.current.instance) return
-        ref.current.instance.init(data)
-        isFirstRun.current = false
-        console.log('init', ref.current.instance)
-      } 
-      else {
-        ref.current.instance.refresh(data)
-        console.log('refresh', ref.current.instance)
-      }
-    }
-  }, [ref, options, data, isLoaded.current])
+  }, [ref, options, data])
   console.log('render')
   
   return <div ref={ref} style={style}></div>

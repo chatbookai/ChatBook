3b86150b4dd939c22b7a44b9411ab18869faaf47
ChatBookAI <chatbookai@gmail.com>
2024-04-28 15:36:22 +0800

    CollectionNewEdit

--- src/views/dataset/edit/CollectionNewEdit.tsx
+++ src/views/dataset/edit/CollectionNewEdit.tsx
@@ -1,18 +1,23 @@
-import { useEffect, memo, Fragment } from 'react'
+import { useEffect, memo, Fragment, useState } from 'react'
 
 import { useRouter } from 'next/router'
 import { useAuth } from 'src/hooks/useAuth'
 import { CheckPermission } from 'src/functions/ChatBook'
 import { useTranslation } from 'react-i18next'
 
-import Dialog from '@mui/material/Dialog'
-import DialogTitle from '@mui/material/DialogTitle'
-import DialogContent from '@mui/material/DialogContent'
-import DialogActions from '@mui/material/DialogActions'
+
+import Step from '@mui/material/Step'
+import Stepper from '@mui/material/Stepper'
+import StepLabel from '@mui/material/StepLabel'
+import StepperCustomDot from './StepperCustomDot'
+import StepperWrapper from 'src/@core/styles/mui/stepper'
 
 
 import Radio from '@mui/material/Radio'
+import Divider from '@mui/material/Divider'
 import RadioGroup from '@mui/material/RadioGroup'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
 import FormControl from '@mui/material/FormControl'
 import FormControlLabel from '@mui/material/FormControlLabel'
 
@@ -32,33 +37,45 @@ import CloseIcon from '@mui/icons-material/Close';
 const DataTypesList: any[] = [
     {
       type: 'File',
-      avatar: '/imgs/module/extract.png',
       name: 'Local file',
       intro: 'Local file desc',
     },
     {
       type: 'Web',
-      avatar: '/imgs/module/http.png',
       name: 'Web link',
       intro: 'Web link desc',
     },
     {
       type: 'Text',
-      avatar: '/imgs/module/variable.png',
       name: 'Custom text',
       intro: 'Custom text',
     },
     {
       type: 'Table',
-      avatar: '/imgs/module/db.png',
       name: 'Table collection',
       intro: 'Table collection',
+    }
+]
+
+const steps = [
+    {
+      title: 'Select Files',
+      subtitle: 'Select Files'
+    },
+    {
+      title: 'Data Deal',
+      subtitle: 'Data Deal'
+    },
+    {
+      title: 'Upload Data',
+      subtitle: 'Upload Data'
     }
 ]
 
 const CollectionNewEdit = (props: any) => {
     // ** Props
     const {pageData, setPageData, handleSubmit, isDisabledButton } = props
+    const [activeStep, setActiveStep] = useState<number>(0)
 
     // ** Hook
     const { t } = useTranslation()
@@ -72,9 +89,11 @@ const CollectionNewEdit = (props: any) => {
     return (
         <Grid container sx={{m: 3, p: 3}}>
             <Grid item sx={{p: 1}} xs={12}>
-                <Box display="flex" mb={1} alignItems="center">
+                <Box display="flex" justifyContent="space-between" alignItems="center" mb={1}>
                     <Typography sx={{pb: 1}}>{t('Dataset Type') as string}</Typography>
-                    <span style={{ paddingTop: '10px', color: 'red', marginLeft: '3px' }}>*</span>
+                    <Button sx={{mr: 3}} size="small" variant='outlined' disabled={isDisabledButton} onClick={() => { setPageData( (prevState: any) => ({ ...prevState, openEdit: false }) ) }}>
+                        {t("Cancel")}
+                    </Button>
                 </Box>
                 <RadioGroup row value={pageData.type} name='simple-radio' aria-label='simple-radio' onClick={(e: any)=>{    
                         if(e.target.value)   {
@@ -88,135 +107,90 @@ const CollectionNewEdit = (props: any) => {
                         return (<FormControlLabel value={item.type} control={<Radio />} label={t(item.name) as string} />)
                     })}
                 </RadioGroup>
-                
             </Grid>
-            <Grid item sx={{pr: 3}} xs={12}>
-                <Grid container alignItems="center">
-                    <Grid item xs={4}>
-                        <InputLabel id='demo-dialog-select-label'>{t("Name")}</InputLabel>
-                    </Grid>
-                    <Grid item xs={8} sx={{pt: 6, pl: 2}}>
-                        <TextField
-                            size="small"
-                            value={pageData.name}
-                            sx={{ width: '100%', resize: 'both', '& .MuiInputBase-input': { fontSize: '0.875rem' } }}
-                            placeholder={t(pageData.placeholder) as string}
-                            onChange={(e: any) => {
-                                setPageData( (prevState: any) => ({ ...prevState, name: e.target.value }) )
-                            }}
-                            />
+            <Grid item sx={{p: 1, mr: 3, pl: 0}} xs={12}>
+                <Card>
+                    <CardContent sx={{}}>
+                        <StepperWrapper>
+                        <Stepper activeStep={activeStep}>
+                            {steps.map((step, index) => {
+                                return (
+                                    <Step key={index}>
+                                        <StepLabel StepIconComponent={StepperCustomDot}>
+                                            <div className='step-label'>
+                                            <Typography className='step-number'>{`${index + 1}`}</Typography>
+                                            <div>
+                                                <Typography className='step-title'>{t(step.title)}</Typography>
+                                            </div>
+                                            </div>
+                                        </StepLabel>
+                                    </Step>
+                                )
+                            })}
+                        </Stepper>
+                        </StepperWrapper>
+                    </CardContent>
+                </Card>
+            </Grid>
+
+            <Divider sx={{ m: '0 !important' }} />
+            
+            {pageData.type == 'Text' ?
+            <Fragment>
+                <Grid item sx={{pr: 3}} xs={12}>
+                    <Grid container alignItems="center">
+                        <Grid item xs={3} sx={{pt: 4}}>
+                            <InputLabel id='demo-dialog-select-label'>{t("Collection Name")}</InputLabel>
+                        </Grid>
+                        <Grid item xs={9} sx={{pt: 6, pl: 2}}>
+                            <TextField
+                                size="small"
+                                value={pageData.name}
+                                sx={{ width: '100%', resize: 'both', '& .MuiInputBase-input': { fontSize: '0.875rem' } }}
+                                placeholder={t(pageData.name) as string}
+                                onChange={(e: any) => {
+                                    setPageData( (prevState: any) => ({ ...prevState, name: e.target.value }) )
+                                }}
+                                />
+                        </Grid>
                     </Grid>
                 </Grid>
-            </Grid>
-            <Grid item sx={{pr: 3}} xs={12}>
-                <Grid container alignItems="center">
-                    <Grid item xs={4}>
-                        <InputLabel id='demo-dialog-select-label'>{t("dataTotal")}</InputLabel>
-                    </Grid>
-                    <Grid item xs={8} sx={{pt: 6, pl: 2}}>
-                        <TextField
-                            type="number"
-                            size="small"
-                            inputProps={{ min: 0, max: 160000 }} 
-                            value={pageData.dataTotal}
-                            sx={{ width: '100%', resize: 'both', '& .MuiInputBase-input': { fontSize: '0.875rem' } }}
-                            placeholder={t(pageData.placeholder) as string}
-                            onChange={(e: any) => {
-                                setPageData( (prevState: any) => ({ ...prevState, dataTotal: e.target.value }) )
-                            }}
-                            />
-                    </Grid>
-                </Grid>
-            </Grid>
-            <Grid item sx={{pr: 3}} xs={12}>
-                <Grid container alignItems="center">
-                    <Grid item xs={4}>
-                        <InputLabel id='demo-dialog-select-label'>{t("status")}</InputLabel>
-                    </Grid>
-                    <Grid item xs={8} sx={{pt: 6, pl: 2}}>
-                        <TextField
-                            type="number"
-                            size="small"
-                            inputProps={{ min: 0, max: 100 }} 
-                            value={pageData.status}
-                            sx={{ width: '100%', resize: 'both', '& .MuiInputBase-input': { fontSize: '0.875rem' } }}
-                            placeholder={t(pageData.placeholder) as string}
-                            onChange={(e: any) => {
-                                setPageData( (prevState: any) => ({ ...prevState, status: e.target.value }) )
-                            }}
-                            />
+                <Grid item sx={{pr: 3}} xs={12}>
+                    <Grid container alignItems="center">
+                        <Grid item xs={3} sx={{pt: 4}}>
+                            <InputLabel id='demo-dialog-select-label'>{t("Collection Content")}</InputLabel>
+                        </Grid>
+                        <Grid item xs={9} sx={{pt: 6, pl: 2}}>
+                            <TextField
+                                multiline
+                                fullWidth
+                                rows={9}
+                                size="small"
+                                value={pageData.name}
+                                sx={{ width: '100%', resize: 'both', '& .MuiInputBase-input': { fontSize: '0.875rem' } }}
+                                placeholder={t(pageData.name) as string}
+                                onChange={(e: any) => {
+                                    setPageData( (prevState: any) => ({ ...prevState, data: e.target.value }) )
+                                }}
+                                />
+                        </Grid>
                     </Grid>
                 </Grid>
-            </Grid>
-            <Grid item sx={{pr: 3}} xs={12}>
-                <Grid container alignItems="center">
-                    <Grid item xs={4}>
-                        <InputLabel id='demo-dialog-select-label'>{t("expiredTime")}</InputLabel>
-                    </Grid>
-                    <Grid item xs={8} sx={{pt: 6, pl: 2}}>
-                        <TextField
-                            size="small"
-                            value={pageData.expiredTime}
-                            sx={{ width: '100%', resize: 'both', '& .MuiInputBase-input': { fontSize: '0.875rem' } }}
-                            placeholder={t(pageData.placeholder) as string}
-                            onChange={(e: any) => {
-                                setPageData( (prevState: any) => ({ ...prevState, expiredTime: e.target.value }) )
-                            }}
-                            />
-                    </Grid>
+                <Grid container sx={{mt: 4, pr: 3, justifyContent: 'flex-end'}} xs={12}>
+                    <Button size="small" variant='contained' disabled={isDisabledButton} onClick={
+                        () => { handleSubmit() }
+                    }>
+                    {t("Next")}
+                    </Button>
                 </Grid>
-            </Grid>
-            <Grid item sx={{pr: 3}} xs={12}>
-                <Grid container alignItems="center">
-                    <Grid item xs={4}>
-                        <InputLabel id='demo-dialog-select-label'>{t("updateTime")}</InputLabel>
-                    </Grid>
-                    <Grid item xs={8} sx={{pt: 6, pl: 2}}>
-                        <Switch 
-                            checked={pageData.updateTime == 1 ? true : false} 
-                            onChange={(e: any) => {
-                                setPageData( (prevState: any) => ({ ...prevState, updateTime: e.target.checked ? 1 : 0 }) )
-                            }} 
-                        />
-                    </Grid>
-                </Grid>
-            </Grid>
-            <Grid container sx={{pr: 3, justifyContent: 'flex-end'}} xs={12}>
-                <Button size="small" variant='outlined' sx={{mr: 3}} disabled={isDisabledButton} onClick={
-                    () => { setPageData( (prevState: any) => ({ ...prevState, openEdit: false }) ) }
-                }>
-                {t("Cancel")}
-                </Button>
-                <Button size="small" variant='contained' disabled={isDisabledButton} onClick={
-                    () => { handleSubmit() }
-                }>
-                {t(pageData.FormSubmit)}
-                </Button>
-            </Grid>
+            </Fragment>
+            :
+            null
+            }
+
+            
         </Grid>
     );
 };
 
 export default memo(CollectionNewEdit);
-
-/*
-
-                <Grid item xs={12}>
-                    <Grid container alignItems="center">
-                        <Grid item xs={4}>
-                            <InputLabel id='demo-dialog-select-label'>{t("authCheck")}</InputLabel>
-                        </Grid>
-                        <Grid item xs={8} sx={{pt: 6, pl: 2}}>
-                            <TextField
-                                size="small"
-                                value={pageData.authCheck}
-                                sx={{ width: '100%', resize: 'both', '& .MuiInputBase-input': { fontSize: '0.875rem' } }}
-                                placeholder={t(pageData.placeholder) as string}
-                                onChange={(e: any) => {
-                                  setPageData( (prevState: any) => ({ ...prevState, authCheck: e.target.value }) )
-                                }}
-                              />
-                        </Grid>
-                    </Grid>
-                </Grid>
-*/

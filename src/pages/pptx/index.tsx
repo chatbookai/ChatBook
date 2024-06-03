57e87ae9ebef188cdbaecc2a86384b5780a55f3a
ChatBookAI <chatbookai@gmail.com>
2024-06-03 18:20:29 +0800

    prosemirror

--- src/pages/pptx/index.tsx
+++ src/pages/pptx/index.tsx
@@ -1,14 +1,15 @@
-// ** React Imports
-import { useState, MouseEvent, Fragment } from 'react'
 
 // ** MUI Components
 import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
+
+import { layouts } from 'src/functions/pptx/data/layouts'
+
 const RegisterV1 = () => {
   // ** Hook
   
-
+  console.log("layouts", layouts)
 
   return (
       <Box className='content-center'>
